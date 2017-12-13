f99de0438ce837a4b39e45cefcf32b21ef3e9eb0
userKarim <alouini.karim@gmail.com>
2017-12-13 18:28:16 +0100

    Added event image

--- SkiWorld-ejb/src/main/java/tn/codeinc/persistance/EventImage.java
+++ SkiWorld-ejb/src/main/java/tn/codeinc/persistance/EventImage.java
@@ -1,6 +1,5 @@
 package tn.codeinc.persistance;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
